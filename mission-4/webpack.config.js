55705ba145fbbf1aeef85b542b068730b6b43bc3
Sangbeom Heo <mansaout@gmail.com>
2022-09-17 23:17:56 +0900

    feat: mission 4 프로젝트 세팅 완료

---
+++ mission-4/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    main: './src/app.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+      },
+    ],
+  },
+};
